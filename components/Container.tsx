294ae948578a217b8baa809f65d50a6faf304fc4
Samson Zhang <wwsamson@yahoo.com>
2021-10-03 23:29:33 -0700

    implement email log in for app store testing

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,19 +5,17 @@ import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
 import {StatusBar} from "expo-status-bar";
 
 export default function Container({children, className, withTabs, noScroll}: { children: ReactNode, className?: string, withTabs?: boolean, noScroll?: boolean }) {
-    const InnerEl = () => (
-        <View style={tw.style("px-4 pt-12", {minHeight: Dimensions.get("window").height - (withTabs ? 72 : 0)}, className)}>
-            {children}
-        </View>
-    );
-
     return (
         <View style={tw`bg-white`}>
             {noScroll ? (
-                <InnerEl/>
+                <View style={tw.style("px-4 pt-12", {minHeight: Dimensions.get("window").height - (withTabs ? 72 : 0)}, className)}>
+                    {children}
+                </View>
             ) : (
                 <KeyboardAwareScrollView>
-                    <InnerEl/>
+                    <View style={tw.style("px-4 pt-12", {minHeight: Dimensions.get("window").height - (withTabs ? 72 : 0)}, className)}>
+                        {children}
+                    </View>
                 </KeyboardAwareScrollView>
             )}
             <StatusBar style="auto"/>
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Container from "../components/Container";
 import H1 from "../components/H1";
-import {Pressable} from "react-native";
+import {Pressable, TextInput, View} from "react-native";
 import BodyText from "../components/BodyText";
 import {StackNavigationProp} from "@react-navigation/stack";
 import {useUser} from "../components/UserProvider";
@@ -11,6 +11,9 @@ import tw from "tailwind-react-native-classnames";
 
 export default function Login({navigation}: {navigation: StackNavigationProp<any>}) {
     const user = useUser();
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     useEffect(() => {
         if (user) {
@@ -36,10 +39,39 @@ export default function Login({navigation}: {navigation: StackNavigationProp<any
         }
     }, [response]);
 
+    const canLoginWithEmail = !!(email && password);
+
+    function loginWithEmail() {
+        if (!canLoginWithEmail) return;
+
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    }
+
     return (
-        <Container>
+        <Container noScroll={true}>
             <H1 className="my-6">Log in</H1>
             <Pressable onPress={() => promptAsync()} style={tw`p-2 bg-gray-100 rounded items-center`}>
+                <BodyText>Sign up/in with Google</BodyText>
+            </Pressable>
+            <View style={tw`mt-8 border-gray-300 border-t w-full h-8`}/>
+            <TextInput
+                value={email}
+                onChangeText={text => setEmail(text)}
+                style={tw.style(`border rounded border-gray-300 p-2 mb-2 text-black`, {fontSize: 16})}
+                placeholder="Email"
+            />
+            <TextInput
+                value={password}
+                onChangeText={text => setPassword(text)}
+                secureTextEntry={true}
+                style={tw.style(`border rounded border-gray-300 p-2 mb-2 text-black`, {fontSize: 16})}
+                placeholder="Password"
+            />
+            <Pressable
+                onPress={() => loginWithEmail()}
+                style={tw`p-2 bg-gray-100 rounded items-center`}
+                disabled={!canLoginWithEmail}
+            >
                 <BodyText>Sign in</BodyText>
             </Pressable>
         </Container>
