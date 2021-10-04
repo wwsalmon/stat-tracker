e079b1418f8134d692fccb997a50da2c2ef9cad9
Samson Zhang <wwsamson@yahoo.com>
2021-10-03 17:11:10 -0700

    mostly fix modal long text bug

--- screens/Home/Log/Log.tsx
+++ screens/Home/Log/Log.tsx
@@ -99,117 +99,119 @@ export default function Log({navigation, route}: { navigation: StackNavigationPr
                 }
                 modalHeaderTitle="New record"
                 modalContainer={
-                    <KeyboardAwareScrollView style={tw.style(`w-full h-full bg-gray-50`, {minHeight: Dimensions.get("window").height - 110})}>
-                        <View style={tw`mt-4`}>
-                            <View style={tw`bg-white w-full px-4 h-12 justify-center`}>
-                                <DateTimePicker
-                                    value={thisDate || new Date()}
-                                    onChange={(e, date) => setThisDate(date as Date)}
-                                    mode="datetime"
-                                />
-                            </View>
-                            <Pressable onPress={() => setThisDate(new Date())} style={tw`bg-white px-4 h-12 justify-center border-t border-gray-100`}>
-                                <BodyText className="text-gray-500">Set to current time</BodyText>
-                            </Pressable>
-                            <Pressable onPress={() => setThisDate(new Date((thisDate || new Date()).setHours(0,0,0,0)))} style={tw`bg-white px-4 h-12 justify-center border-t border-gray-100`}>
-                                <BodyText className="text-gray-500">Set to 0:00</BodyText>
-                            </Pressable>
-                        </View>
-                        <View style={tw`my-4`}>
-                            <TextInput
-                                style={tw.style(`h-12 bg-white px-4`, {fontSize: 16})}
-                                placeholderTextColor="#999999"
-                                placeholder="Search for stat to add"
-                                value={searchQuery}
-                                onChangeText={text => setSearchQuery(text)}
-                            />
-                            {stats.map(stat => (
-                                <Pressable
-                                    key={stat.id}
-                                    style={tw`h-12 bg-white px-4 flex-row items-center border-t border-gray-100`}
-                                    onPress={() => {
-                                        setSearchQuery("");
-                                        setThisStats([{...stat, value: stat.type === "note" ? "" : 0}, ...thisStats]);
-                                    }}
-                                >
-                                    <View style={tw`w-6 h-6 mr-4 bg-green-400 rounded-full items-center justify-center`}>
-                                        <Feather name="plus" color="white" size={16}/>
-                                    </View>
-                                    <BodyText>{stat.name}</BodyText>
+                    <View style={tw.style(`w-full bg-gray-50`, {height: Dimensions.get("window").height - 110})}>
+                        <KeyboardAwareScrollView>
+                            <View style={tw`mt-4`}>
+                                <View style={tw`bg-white w-full px-4 h-12 justify-center`}>
+                                    <DateTimePicker
+                                        value={thisDate || new Date()}
+                                        onChange={(e, date) => setThisDate(date as Date)}
+                                        mode="datetime"
+                                    />
+                                </View>
+                                <Pressable onPress={() => setThisDate(new Date())} style={tw`bg-white px-4 h-12 justify-center border-t border-gray-100`}>
+                                    <BodyText className="text-gray-500">Set to current time</BodyText>
                                 </Pressable>
-                            ))}
-                            <Text style={tw`text-gray-400 ml-4 mt-2`}>Create new stats through the "Stats" tab</Text>
-                        </View>
-                        {thisStats.map((stat, i) => (
-                            <React.Fragment key={stat.id}>
-                                <View style={tw`px-3 h-12 border flex-row items-center bg-white border-gray-100 px-4`}>
+                                <Pressable onPress={() => setThisDate(new Date((thisDate || new Date()).setHours(0,0,0,0)))} style={tw`bg-white px-4 h-12 justify-center border-t border-gray-100`}>
+                                    <BodyText className="text-gray-500">Set to 0:00</BodyText>
+                                </Pressable>
+                            </View>
+                            <View style={tw`my-4`}>
+                                <TextInput
+                                    style={tw.style(`h-12 bg-white px-4`, {fontSize: 16})}
+                                    placeholderTextColor="#999999"
+                                    placeholder="Search for stat to add"
+                                    value={searchQuery}
+                                    onChangeText={text => setSearchQuery(text)}
+                                />
+                                {stats.map(stat => (
                                     <Pressable
-                                        style={tw`h-full justify-center`}
+                                        key={stat.id}
+                                        style={tw`h-12 bg-white px-4 flex-row items-center border-t border-gray-100`}
                                         onPress={() => {
-                                            let newStats = [...thisStats];
-                                            newStats.splice(i, 1);
-                                            setThisStats(newStats);
+                                            setSearchQuery("");
+                                            setThisStats([{...stat, value: stat.type === "note" ? "" : 0}, ...thisStats]);
                                         }}
                                     >
-                                        <View style={tw`w-6 h-6 mr-4 bg-red-400 rounded-full items-center justify-center`}>
-                                            <Feather name="minus" color="white" size={16}/>
+                                        <View style={tw`w-6 h-6 mr-4 bg-green-400 rounded-full items-center justify-center`}>
+                                            <Feather name="plus" color="white" size={16}/>
                                         </View>
+                                        <BodyText>{stat.name}</BodyText>
                                     </Pressable>
-                                    <BodyText>{stat.name}</BodyText>
-                                    {stat.type !== "note" && (
-                                        <View style={tw`ml-auto`}>
-                                            {stat.type === "score" ? (
-                                                <RNPickerSelect
-                                                    placeholder={{label: "Score", value: null}}
-                                                    style={{
-                                                        inputIOS: tw.style(`w-24 border-l border-gray-100 pl-4 h-12`, {fontSize: 16}),
-                                                        inputAndroid: tw.style(`w-24 border-l border-gray-100 h-12`, {fontSize: 16})
-                                                    }}
-                                                    onValueChange={(value) => {
-                                                        let newStats = [...thisStats];
-                                                        let newStat = {...stat};
-                                                        newStat.value = +value;
-                                                        newStats.splice(i, 1, newStat);
-                                                        setThisStats(newStats);
-                                                    }}
-                                                    items={Array(7).fill(0).map((d, i) => ({label: (i + 1).toString(), value: (i + 1).toString()}))}
-                                                />
-                                            ) : (
-                                                <TextInput
-                                                    style={tw.style(`w-24 border-l border-gray-100 pl-4 h-12`, {fontSize: 16})}
-                                                    placeholder="Number"
-                                                    keyboardType="numeric"
-                                                    value={stat.value.toString()}
-                                                    onChangeText={text => {
-                                                        let newStats = [...thisStats];
-                                                        let newStat = {...stat};
-                                                        newStat.value = +text;
-                                                        newStats.splice(i, 1, newStat);
-                                                        setThisStats(newStats);
-                                                    }}
-                                                />
-                                            )}
-                                        </View>
+                                ))}
+                                <Text style={tw`text-gray-400 ml-4 mt-2`}>Create new stats through the "Stats" tab</Text>
+                            </View>
+                            {thisStats.map((stat, i) => (
+                                <React.Fragment key={stat.id}>
+                                    <View style={tw`px-3 h-12 border flex-row items-center bg-white border-gray-100 px-4`}>
+                                        <Pressable
+                                            style={tw`h-full justify-center`}
+                                            onPress={() => {
+                                                let newStats = [...thisStats];
+                                                newStats.splice(i, 1);
+                                                setThisStats(newStats);
+                                            }}
+                                        >
+                                            <View style={tw`w-6 h-6 mr-4 bg-red-400 rounded-full items-center justify-center`}>
+                                                <Feather name="minus" color="white" size={16}/>
+                                            </View>
+                                        </Pressable>
+                                        <BodyText>{stat.name}</BodyText>
+                                        {stat.type !== "note" && (
+                                            <View style={tw`ml-auto`}>
+                                                {stat.type === "score" ? (
+                                                    <RNPickerSelect
+                                                        placeholder={{label: "Score", value: null}}
+                                                        style={{
+                                                            inputIOS: tw.style(`w-24 border-l border-gray-100 pl-4 h-12`, {fontSize: 16}),
+                                                            inputAndroid: tw.style(`w-24 border-l border-gray-100 h-12`, {fontSize: 16})
+                                                        }}
+                                                        onValueChange={(value) => {
+                                                            let newStats = [...thisStats];
+                                                            let newStat = {...stat};
+                                                            newStat.value = +value;
+                                                            newStats.splice(i, 1, newStat);
+                                                            setThisStats(newStats);
+                                                        }}
+                                                        items={Array(7).fill(0).map((d, i) => ({label: (i + 1).toString(), value: (i + 1).toString()}))}
+                                                    />
+                                                ) : (
+                                                    <TextInput
+                                                        style={tw.style(`w-24 border-l border-gray-100 pl-4 h-12`, {fontSize: 16})}
+                                                        placeholder="Number"
+                                                        keyboardType="numeric"
+                                                        value={stat.value.toString()}
+                                                        onChangeText={text => {
+                                                            let newStats = [...thisStats];
+                                                            let newStat = {...stat};
+                                                            newStat.value = +text;
+                                                            newStats.splice(i, 1, newStat);
+                                                            setThisStats(newStats);
+                                                        }}
+                                                    />
+                                                )}
+                                            </View>
+                                        )}
+                                    </View>
+                                    {stat.type === "note" && (
+                                        <TextInput
+                                            multiline={true}
+                                            style={tw.style(`bg-white text-base px-4 pt-2 pb-3 border-b border-gray-100`, {minHeight: 128})}
+                                            placeholder="Add note: an explanation, context, etc."
+                                            value={stat.value.toString()}
+                                            onChangeText={text => {
+                                                let newStats = [...thisStats];
+                                                let newStat = {...stat};
+                                                newStat.value = text;
+                                                newStats.splice(i, 1, newStat);
+                                                setThisStats(newStats);
+                                            }}
+                                        />
                                     )}
-                                </View>
-                                {stat.type === "note" && (
-                                    <TextInput
-                                        multiline={true}
-                                        style={tw.style(`bg-white text-base px-4 pt-2 pb-3 border-b border-gray-100`, {minHeight: 128})}
-                                        placeholder="Add note: an explanation, context, etc."
-                                        value={stat.value.toString()}
-                                        onChangeText={text => {
-                                            let newStats = [...thisStats];
-                                            let newStat = {...stat};
-                                            newStat.value = text;
-                                            newStats.splice(i, 1, newStat);
-                                            setThisStats(newStats);
-                                        }}
-                                    />
-                                )}
-                            </React.Fragment>
-                        ))}
-                    </KeyboardAwareScrollView>
+                                </React.Fragment>
+                            ))}
+                        </KeyboardAwareScrollView>
+                    </View>
                 }
                 modalType="iOS Form Sheet"
                 modalVisible={modalOpen}
