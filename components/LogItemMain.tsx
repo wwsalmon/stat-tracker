247d761f3911fbbb521b04aece793176696453ec
Samson Zhang <wwsamson@yahoo.com>
2021-10-04 00:07:02 -0700

    fix log item page scrolling

--- components/LogItemMain.tsx
+++ components/LogItemMain.tsx
@@ -108,22 +108,24 @@ export default function LogItemMain({navigation, route, setModalOpen, iter, reco
             </View>
             {record ? (
                 <ScrollView>
-                    <Text style={tw`text-xl font-bold mb-6`}>Record from {format(new Date(record.createdAt.seconds * 1000), "MMMM d, yyyy 'at' h:mm aa")}</Text>
-                    {numberStats && numberStats.map((stat, i) => (
-                        <View
-                            style={tw.style(`h-12 flex-row items-center px-4`, {backgroundColor: stat.color}, i === 0 && "rounded-t", i === numberStats.length - 1 && (textStats && textStats.length === 0) && "rounded-b")}
-                            key={record.id + stat.id}
-                        >
-                            <Text style={tw`text-white w-8 font-bold`}>{stat.value}</Text>
-                            <Text style={tw`text-white`}>{stat.name}</Text>
-                        </View>
-                    ))}
-                    {textStats && textStats.map((stat, i) => (
-                        <View key={record.id + stat.id} style={tw.style(`px-4 py-3`, {backgroundColor: stat.color}, i === textStats.length - 1 && "rounded-b")}>
-                            <Text style={tw`text-gray-300 my-3`}>{stat.name}</Text>
-                            <BodyText className="text-white mb-3">{stat.value}</BodyText>
-                        </View>
-                    ))}
+                    <View style={tw`pb-60`}>
+                        <Text style={tw`text-xl font-bold mb-6`}>Record from {format(new Date(record.createdAt.seconds * 1000), "MMMM d, yyyy 'at' h:mm aa")}</Text>
+                        {numberStats && numberStats.map((stat, i) => (
+                            <View
+                                style={tw.style(`h-12 flex-row items-center px-4`, {backgroundColor: stat.color}, i === 0 && "rounded-t", i === numberStats.length - 1 && (textStats && textStats.length === 0) && "rounded-b")}
+                                key={record.id + stat.id}
+                            >
+                                <Text style={tw`text-white w-8 font-bold`}>{stat.value}</Text>
+                                <Text style={tw`text-white`}>{stat.name}</Text>
+                            </View>
+                        ))}
+                        {textStats && textStats.map((stat, i) => (
+                            <View key={record.id + stat.id} style={tw.style(`px-4 py-3`, {backgroundColor: stat.color}, i === textStats.length - 1 && "rounded-b")}>
+                                <Text style={tw`text-gray-300 my-3`}>{stat.name}</Text>
+                                <BodyText className="text-white mb-3">{stat.value}</BodyText>
+                            </View>
+                        ))}
+                    </View>
                 </ScrollView>
             ) : (
                 <BodyText>Loading...</BodyText>
